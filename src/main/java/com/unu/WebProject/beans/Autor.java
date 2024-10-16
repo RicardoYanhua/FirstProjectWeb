8a30faebea4e34b320c58ed0e99611629be3bd59
unknown <ricardoyanhua@gmail.com>
2024-10-16 16:47:32 -0500

    first commit

---
+++ src/main/java/com/unu/WebProject/beans/Autor.java
@@ -0,0 +1,45 @@
+package com.unu.WebProject.beans;
+
+public class Autor {
+
+	// Variables
+	public int idAutor;
+	private String nacionalidad;
+	private String nombre;
+
+	public Autor() {
+		this(0, "", "");
+	}
+
+	public Autor(int idAutor, String nacionalidad, String nombre) {
+		super();
+		this.idAutor = idAutor;
+		this.nacionalidad = nacionalidad;
+		this.nombre = nombre;
+	}
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ src/main/java/com/unu/WebProject/models/AutoresModel.java
@@ -0,0 +1,22 @@
+package com.unu.WebProject.models;
+
+import java.util.*;
+
+
+import com.unu.WebProject.beans.*;
+
+public class AutoresModel {
+
+	public List<Autor> LISTA_AUTORES (){
+		
+		ArrayList<Autor> autores = new ArrayList<Autor>();
+		autores.add(new Autor (1 , "Garcia Marquez","Colombiano"));
+		autores.add(new Autor (2 , "Borges","Argentino"));
+		autores.add(new Autor (3 , "Allende","Chileno"));
+		
+		
+		
+		return autores;
+	}
+	
+}
